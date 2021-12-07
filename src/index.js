d522a4c3a564fb05195b99e810d11bad7181b16c
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-12-07 23:27:48 +0530

    imported actions and reducers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from'./components/App';
+import {Provider} from 'react-redux';
+import rootReducer from './reducers';
+import {createStore} from 'redux';
+import { addCharecterById } from './actions';
 
+const store=createStore(rootReducer);
+console.log('store.getState()' ,store.getState());
+store.subscribe (()=>console.log('store',store.getState()));
+store.dispatch(addCharecterById(2));
 ReactDOM.render( 
-<App/>,
+ <Provider store={store}>  
+<App/>
+</Provider> ,
 document.getElementById('root'));
    
 
