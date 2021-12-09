75999cbe4614f7372f0c71c91ef4d902f556031b
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-12-10 00:26:40 +0530

    rendered new component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React,{Component} from 'react';
+import CharecterList from './CharecterList';
 export default class App extends React.Component{
     render(){
         return(
             <div>
                 SquadProject
+                <CharecterList/>
             </div>
+            
         )
     }
 
