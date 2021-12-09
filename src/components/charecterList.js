094f241dd34a41f6f5a5e114edad2ccaf0003ee9
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-12-09 17:37:29 +0530

    component for map state to props

---
+++ src/components/charecterList.js
@@ -0,0 +1,9 @@
+import React ,{Component} from 'react';
+import {connect} from 'react-redux';
+function charecterList (){
+    return(
+        <div>
+            charecterList
+        </div>
+    )
+}
