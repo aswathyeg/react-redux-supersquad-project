38ecc9e39e1991d5e3ead9d61376d881b4167f5f
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-12-10 00:27:15 +0530

    updated

--- src/reducers/helpers.js
+++ src/reducers/helpers.js
@@ -1,6 +1,6 @@
 import charecters_json from '../data/charecters.json';
 
 export function createCharecter(id){
-    let charecter=charecters.charecters_json.find(c=>c.id===id);
+    let charecter=charecters_json.find(c=>c.id===id);
     return charecter;
 }
