d4cbab13db10969ce9580b7a0a08ba8b49f6479d
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-12-10 18:08:06 +0530

    added remove charecter

--- src/reducers/heros_reducer.js
+++ src/reducers/heros_reducer.js
@@ -1,14 +1,18 @@
 import charecters_json from '../data/charecters.json';
-import {ADD_CHARECTER} from '../actions';
+import {ADD_CHARECTER,REMOVE_CHARECTER} from '../actions';
 import {createCharecter} from './helpers';
 
-function heros(state=[],action){
+function heroes(state=[],action){
     switch (action.type){
-        case'ADD_CHARECTER':
-        let heros=[...state,createCharecter(action.id)];
-        return heros;
-        default:
+        case ADD_CHARECTER:
+        let heroes=[...state,createCharecter(action.id)];
+        return heroes;
+       
+            case REMOVE_CHARECTER:
+                heroes= state.filter(item=>item.id!==action.id);
+                return heroes;
+                default:
             return state;
     }
 }
-export default heros;
+export default heroes;
