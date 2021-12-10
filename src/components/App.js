7ad8ce1f795d4c656d81bf6385e7d2562d0eeeb0
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-12-10 18:01:37 +0530

    added style

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React,{Component} from 'react';
 import CharecterList from './CharecterList';
+import HeroList from './HeroList';
+
+import '../styles/index.css';
 export default class App extends React.Component{
     render(){
         return(
-            <div>
+            <div className="App">
                 SquadProject
                 <CharecterList/>
+                
+                <HeroList/>
+                
             </div>
             
         )
